5278a51567c438885ee8abf7d329072aea1d7046
Kenneth <kennethzhang374@gmail.com>
2020-10-24 21:24:08 -0700

    adding header

--- src/App.js
+++ src/App.js
@@ -1,31 +1,35 @@
 import React from "react";
-import { MenuItem, FormControl, Selection } from "@,ateroa;-ui/core";
+import { MenuItem, FormControl, Select } from "@material-ui/core";
 import "./App.css";
 
 function App() {
   return (
-    <div className="app">
-      <h1>Covid-19 Tracker</h1>
-      <FormControl className="app__dropdown">
-        <Selection variant="outlined" value="abc">
-          <MenuItem value="worldwide">WorldWide</MenuItem>
-          <MenuItem value="worldwide">Option Two</MenuItem>
-          <MenuItem value="worldwide">Option 3</MenuItem>
-          <MenuItem value="worldwide">Yoooo</MenuItem>
-        </Selection>
-      </FormControl>
+    <>
+      <div className="app">
+        <div className="app__header">
+          <h1>COVID-19 TRACKER</h1>
+          <FormControl className="app__dropdown">
+            <Select variant="outlined" value="abc">
+              <MenuItem value="worldwide">WorldWide</MenuItem>
+              <MenuItem value="worldwide">Option Two</MenuItem>
+              <MenuItem value="worldwide">Option 3</MenuItem>
+              <MenuItem value="worldwide">Yoooo</MenuItem>
+            </Select>
+          </FormControl>
+        </div>
 
-      {/* {Header} */}
-      {/* Title + select input dropdown field */}
-      {/* {info box} */}
-      {/* {info box} */}
-      {/* {info box} */}
+        {/* {Header} */}
+        {/* Title + select input dropdown field */}
+        {/* {info box} */}
+        {/* {info box} */}
+        {/* {info box} */}
 
-      {/* Table on the side bar */}
-      {/* Grapj below the table */}
+        {/* Table on the side bar */}
+        {/* Grapj below the table */}
 
-      {/* {map at the bottom} */}
-    </div>
+        {/* {map at the bottom} */}
+      </div>
+    </>
   );
 }
 
