5c8e3c75005f9a1028d4de55b489bcda5addaf0d
Kenneth <kennethzhang374@gmail.com>
2020-10-24 20:25:21 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <div className="app">
-      <h1>Lets build a covid 19 tracker clever programmers!!!</h1>
+      <h1>Lets build a covid 19 tracker clever programmers!!!!!</h1>
     </div>
   );
 }
