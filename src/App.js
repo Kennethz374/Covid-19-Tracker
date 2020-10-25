a59cb5131412b7b2cfa6eef6b191312e9fd2cf68
Kenneth <kennethzhang374@gmail.com>
2020-10-24 20:35:08 -0700

    structure out all components need to be made for the app

--- src/App.js
+++ src/App.js
@@ -4,6 +4,17 @@ function App() {
   return (
     <div className="app">
       <h1>Lets build a covid 19 tracker clever programmers!!!!!</h1>
+
+      {/* {Header} */}
+      {/* Title + select input dropdown field */}
+      {/* {info box} */}
+      {/* {info box} */}
+      {/* {info box} */}
+
+      {/* Table on the side bar */}
+      {/* Grapj below the table */}
+
+      {/* {map at the bottom} */}
     </div>
   );
 }
