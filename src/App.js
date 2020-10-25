ffeae2f20aaaf1761aef6bfd0952610bdb837a87
Kenneth <kennethzhang374@gmail.com>
2020-10-24 21:02:16 -0700

    testing drop down

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import React from "react";
+import { MenuItem, FormControl, Selection } from "@,ateroa;-ui/core";
 import "./App.css";
 
 function App() {
   return (
     <div className="app">
-      <h1>Lets build a covid 19 tracker clever programmers!!!!!</h1>
+      <h1>Covid-19 Tracker</h1>
+      <FormControl className="app__dropdown">
+        <Selection variant="outlined" value="abc">
+          <MenuItem value="worldwide">WorldWide</MenuItem>
+          <MenuItem value="worldwide">Option Two</MenuItem>
+          <MenuItem value="worldwide">Option 3</MenuItem>
+          <MenuItem value="worldwide">Yoooo</MenuItem>
+        </Selection>
+      </FormControl>
 
       {/* {Header} */}
       {/* Title + select input dropdown field */}
