5370725c29282f32d0659d08108ab26f6bf6b65e
Kenneth <kennethzhang374@gmail.com>
2020-11-03 21:24:58 -0800

    info box component made

--- src/InfoBox.js
+++ src/InfoBox.js
@@ -1,8 +1,26 @@
 // rfce
 import React from "react";
+import { Card, CardContent, Typography } from "@material-ui/core";
 
 function InfoBox({ title, cases, total }) {
-  return <div></div>;
+  return (
+    <Card className="infoBox">
+      <CardContent>
+        {/* Title Coronavirus */}
+        <Typography className="infoBox__title" color="textSecondary">
+          {title}
+        </Typography>
+
+        {/*  + 120k Number of cases */}
+        <h2 className="infoBox__cases">{cases}</h2>
+
+        {/* 1.2M total */}
+        <Typography clasName="infoBox__total" color="textSecondary">
+          {total} total
+        </Typography>
+      </CardContent>
+    </Card>
+  );
 }
 
 export default InfoBox;
