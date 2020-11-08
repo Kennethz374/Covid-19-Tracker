79676670fa1cec950f5a4ea4bd919990c20469e4
Kenneth <kennethzhang374@gmail.com>
2020-11-07 22:34:38 -0800

    added helper function to sort out number of cases

--- src/helper.js
+++ src/helper.js
@@ -1,14 +1,13 @@
 export const sortData = data => {
   const sortedData = [...data];
+  return sortedData.sort((a, b) => (a.cases > b.cases ? -1 : 1));
 
-  sortedData.sort((a, b) => {
-    if (a.cases > b.cases) {
-      return -1;
-    } else {
-      return 1;
-    }
-  });
-  return sortedData;
+  // sortedData.sort((a, b) => {
+  //   if (a.cases > b.cases) {
+  //     return -1;
+  //   } else {
+  //     return 1;
+  //   }
+  // });
+  // return sortedData;
 };
-
-// return sortedData.sort((a,b) => (a.cases > b.cases ? -1:1))  tenory is neat
