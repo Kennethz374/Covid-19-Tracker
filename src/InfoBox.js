0403712c5badd0affb788f8a933ab58b3c0f62ec
Kenneth <kennethzhang374@gmail.com>
2020-11-09 16:14:31 -0800

    put up some more css

--- src/InfoBox.js
+++ src/InfoBox.js
@@ -2,19 +2,20 @@
 import React from "react";
 import { Card, CardContent, Typography } from "@material-ui/core";
 
-function InfoBox({ title, cases, total, ...props }) {
+function InfoBox({ title, cases, total, active, isRed, ...props }) {
   return (
-    <Card className="infoBox" onClick={props.onClick}>
+    <Card
+      className={`infoBox ${active && "infoBox--selected"} ${isRed &&
+        "infoBox--red"}`}
+      onClick={props.onClick}
+    >
       <CardContent>
-        {/* Title Coronavirus */}
         <Typography className="infoBox__title" color="textSecondary">
           {title}
         </Typography>
-
-        {/*  + 120k Number of cases */}
-        <h2 className="infoBox__cases">{cases}</h2>
-
-        {/* 1.2M total */}
+        <h2 className={`infoBox__cases ${!isRed && "infoBox__cases--green"}`}>
+          {cases}
+        </h2>
         <Typography className="infoBox__total" color="textSecondary">
           {total} total
         </Typography>
